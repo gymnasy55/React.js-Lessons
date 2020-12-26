222252356be3b89b844c98fd1e4b21bff90a9ced
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-26 21:03:54 +0200

    1

---
+++ 01-first-projects/react-1/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <div>one div</div>
+            <div className="App">
+                <ul>
+                    <li>html</li>
+                    <li>css</li>
+                    <li>js</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
