fc9f6fe1b01d984f4bc0e55c68c6ae150899169e
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-27 15:32:07 +0200

    22

--- 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
+++ 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
                 <div className={classes.dialog}>
-                    Ilya
+                    <NavLink to="/dialogs/1" activeClassName={classes.active}>Ilya</NavLink>
                 </div>
                 <div className={classes.dialog + ' ' + classes.active}>
-                    Dima
+                    <NavLink to="/dialogs/2" activeClassName={classes.active}>Dima</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Sirgay
+                    <NavLink to="/dialogs/3" activeClassName={classes.active}>Sirgay</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Pasha
+                    <NavLink to="/dialogs/4" activeClassName={classes.active}>Pasha</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Gleb
+                    <NavLink to="/dialogs/5" activeClassName={classes.active}>Gleb</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Danya
+                    <NavLink to="/dialogs/6" activeClassName={classes.active}>Danya</NavLink>
                 </div>
             </div>
             <div className={classes.messages}>
