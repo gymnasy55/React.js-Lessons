ca54d54903cd4e8ef64a2fd9b5b2578413259b61
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-27 15:06:25 +0200

    21

--- 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
+++ 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog}>
+                    Ilya
+                </div>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dima
+                </div>
+                <div className={classes.dialog}>
+                    Sirgay
+                </div>
+                <div className={classes.dialog}>
+                    Pasha
+                </div>
+                <div className={classes.dialog}>
+                    Gleb
+                </div>
+                <div className={classes.dialog}>
+                    Danya
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hey</div>
+                <div className={classes.message}>Bro</div>
+            </div>
         </div>
     )
 }
