810a3c7b58481230a65247e81592eea1064e645d
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-29 17:10:18 +0200

    29

--- 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
+++ 01-first-projects/react-1/src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={classes.dialogs}>
---
+++ 01-first-projects/react-1/src/redux/state.js
@@ -0,0 +1,30 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hello', likesCount: 15},
+            {id: 2, message: 'It\'s my first post', likesCount: 20},
+            {id: 3, message: 'Blabla', likesCount: 11},
+            {id: 4, message: 'Dada', likesCount: 11},
+        ]
+    },
+    dialogsPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Hey'},
+            {id: 3, message: 'Bro'},
+            {id: 4, message: 'Bro'},
+            {id: 5, message: 'Bro'},
+            {id: 6, message: 'Bro'}
+        ],
+        dialogs: [
+            {id: 1, name: 'Ilya'},
+            {id: 2, name: 'Dima'},
+            {id: 3, name: 'Sirgay'},
+            {id: 4, name: 'Pasha'},
+            {id: 5, name: 'Gleb'},
+            {id: 6, name: 'Danya'}
+        ]
+    }
+};
+
+export default state;
