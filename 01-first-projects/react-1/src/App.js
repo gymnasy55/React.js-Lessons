16a454b60436835f6f726024de963d2ed5055e71
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-30 22:57:20 +0200

    43

--- 01-first-projects/react-1/src/App.js
+++ 01-first-projects/react-1/src/App.js
@@ -5,10 +5,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer/Footer";
-import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
     return (
@@ -16,14 +16,8 @@ const App = (props) => {
             <Header/>
             <Navbar/>
             <div className="app-wrapper-content">
-                {/*<Route path="/profile" component={Profile}/>*/}
-                {/*<Route path="/dialogs" component={Dialogs}/>*/}
-                {/*<Route path="/news" component={News}/>*/}
-                {/*<Route path="/music" component={Music}/>*/}
-                {/*<Route path="/settings" component={Settings}/>*/}
-
-                <Route path="/profile" render={() => <Profile state={props.state.profilePage} dispatch={props.dispatch}/>}/>
-                <Route path="/dialogs" render={() => <Dialogs store={props.store}/>}/>
+                <Route path="/profile" render={() => <Profile store={props.store}/>}/>
+                <Route path="/dialogs" render={() => <DialogsContainer store={props.store}/>}/>
                 <Route path="/news" render={() => <News/>}/>
                 <Route path="/music" render={() => <Music/>}/>
                 <Route path="/settings" render={() => <Settings/>}/>
--- 01-first-projects/react-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 01-first-projects/react-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
@@ -9,12 +8,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch(addPostActionCreator());
+        props.addPost();
     }
 
     let onPostChange = () => {
-        let action = updateNewPostTextActionCreator(newPostElement.current.value);
-        props.dispatch(action);
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
     }
 
     return (
