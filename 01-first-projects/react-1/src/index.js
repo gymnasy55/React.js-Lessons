94a87a56b7ef0e3cd6b1fb3e358ca480ad57e764
Kubariev Ilya <polcrazpolcraz@gmail.com>
2020-12-30 21:47:13 +0200

    42 REDUX

--- 01-first-projects/react-1/src/index.js
+++ 01-first-projects/react-1/src/index.js
@@ -1,5 +1,5 @@
 import reportWebVitals from './reportWebVitals';
-import store from './redux/state';
+import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -16,7 +16,9 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    rerenderEntireTree(store.getState());
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- 01-first-projects/react-1/src/redux/dialogs-reducer.js
+++ 01-first-projects/react-1/src/redux/dialogs-reducer.js
@@ -1,7 +1,27 @@
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Hey'},
+        {id: 3, message: 'Bro'},
+        {id: 4, message: 'Bro'},
+        {id: 5, message: 'Bro'},
+        {id: 6, message: 'Bro'}
+    ],
+    dialogs: [
+        {id: 1, name: 'Ilya'},
+        {id: 2, name: 'Dima'},
+        {id: 3, name: 'Sirgay'},
+        {id: 4, name: 'Pasha'},
+        {id: 5, name: 'Gleb'},
+        {id: 6, name: 'Danya'}
+    ],
+    newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_TEXT:
             state.newMessageBody = action.body;
